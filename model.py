2c1a3efeaa985c327ad47101a56435b1c90dc4cd
ayodele-spencer <nerdydreams92@gmail.com>
2022-06-04 17:18:34 +0100

    Set database URL

--- model.py
+++ model.py
@@ -127,7 +127,7 @@ def connect_to_db(flask_app):
     elif os.environ.get("LOCAL_DEV"):
         flask_app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:@localhost:5433/postgres"
     else:  
-        flask_app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "postgresql://postgres:@localhost:5433/postgres")
+        flask_app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "postgresql://postgres:@localhost:5432/postgres")
     
     #additional rules:
     flask_app.config['SQLALCHEMY_ECHO'] = False
